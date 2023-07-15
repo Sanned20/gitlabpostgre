68e954f57fb6c1b3834903a1e5b591583e1767d4
Sanned20 <alexander.ryvkin.97@gmail.com>
2023-07-15 13:38:26 +0300

    add docker install commands

--- docker_install.sh
+++ docker_install.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 password="ubuntu"
 sudo apt update
+sudo apt install curl software-properties-common ca-certificates apt-transport-https -y
+curl -f -s -S -L https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable"
+sudo apt update
+sudo apt install docker-ce -y
